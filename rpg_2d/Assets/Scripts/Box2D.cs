e0c14160fa71a9f6571b4c09ec74a9d524d70dab
lichao2018 <13070153251@163.com>
2018-12-22 22:37:10 +0800

    add box

---
+++ rpg_2d/Assets/Scripts/Box2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            print("collid player");
+            transform.Translate(new Vector3(collision.gameObject.transform.position.x, transform.position.y, transform.position.z));
+        }
+    }
+}
