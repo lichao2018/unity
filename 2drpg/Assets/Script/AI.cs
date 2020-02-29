5f8096303f7626c68623da7195cffb3c00a9045e
lichao2018 <13070153251@163.com>
2020-02-29 23:04:28 +0800

    解决敌人攻击时移动的问题

--- 2drpg/Assets/Script/AI.cs
+++ 2drpg/Assets/Script/AI.cs
@@ -6,12 +6,13 @@ public class AI : MonoBehaviour
 {
     public float moveSpeed;
     public float moveRange;
-    public float attackRange;
 
     Rigidbody2D rig;
     Animator animator;
     float initX;
     bool towardsRight = false;
+    bool attacking = false;
+    bool startAttack = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,21 +25,59 @@ public class AI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(player in attack size){
-        //    attack();
-        //}else{
-        float x = transform.position.x;
-        if (x <= initX - moveRange || x >= initX + moveRange)
-        {
-            towardsRight = !towardsRight;
+        if(attackAnimating() && startAttack){
+            attacking = true;
         }
-        move();
+        if(attacking){
+            rig.velocity = new Vector2(0, rig.velocity.y);
+            if (!attackAnimating()){
+                Debug.Log("stop attack");
+                startAttack = false;
+                attacking = false;
+                animator.SetBool("attack_r", false);
+            }
+        }else{
+            float x = transform.position.x;
+            if (x <= initX - moveRange || x >= initX + moveRange)
+            {
+                towardsRight = !towardsRight;
+            }
+            move();
+        }
+
+        //if (!attackAnimating()){
+        //    animator.SetBool("attack_r", false);
+        //    float x = transform.position.x;
+        //    if (x <= initX - moveRange || x >= initX + moveRange)
+        //    {
+        //        towardsRight = !towardsRight;
+        //    }
+        //    move();
         //}
     }
 
     void move(){
         rig.velocity = new Vector2(towardsRight?moveSpeed:moveSpeed*-1, rig.velocity.y);
-        animator.SetBool("walk_r", towardsRight);
-        animator.SetBool("walk_l", !towardsRight);
+        //animator.SetBool("walk_r", towardsRight);
+        //animator.SetBool("walk_l", !towardsRight);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player"){
+            //todo player damage
+        }
+    }
+
+    public void attack(){
+        //animator.SetBool("walk_r", false);
+        //animator.SetBool("walk_l", false);
+        startAttack = true;
+        animator.SetBool("attack_r", true);
+    }
+
+    bool attackAnimating(){
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        return (stateInfo.IsName("enemyAttackLeft") || stateInfo.IsName("enemyAttackRight")) && stateInfo.normalizedTime < 1.0f;
     }
 }
---
+++ 2drpg/Assets/Script/AttackCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCollider : MonoBehaviour
+{
+    AI ai;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = GetComponentInParent<AI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && ai != null){
+            ai.attack();
+        }
+    }
+}
